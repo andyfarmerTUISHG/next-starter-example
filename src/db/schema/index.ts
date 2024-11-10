cb5f2a7bab00bad024755bbcdeb79588d1029757
Andy Farmer <andy.farmer@travelopia.com>
2024-11-10 21:11:24 +0000

    GuestBook - Add GuestBookeEntries Schema

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,3 +1,5 @@
 export { default as accounts } from "./accounts";
+export { default as guestbookEntries } from "./guestbook-entry";
 export { default as sessions } from "./sessions";
 export { default as users } from "./users";
+
