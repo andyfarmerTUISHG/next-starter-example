f4e4124f943ee0147baa049c4144e8c0f2593ffc
Andy Farmer <andy.farmer@travelopia.com>
2024-11-11 18:05:39 +0000

    GuestBook - Add Export of Relationship to Schema

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,5 +1,5 @@
 export { default as accounts } from "./accounts";
-export { default as guestbookEntries } from "./guestbook-entry";
+export { default as guestbookEntries, guestbookEntriesRelationships } from "./guestbook-entries";
 export { default as sessions } from "./sessions";
 export { default as users } from "./users";
 
