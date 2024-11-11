a7a59d394a86aba1686a9b3fd98d6afec58dbc47
Andy Farmer <andy.farmer@travelopia.com>
2024-11-11 18:02:27 +0000

    GuestBook - Update Schema to have a relationship with User and pull the user data

--- src/db/schema/guestbook-entries.ts
+++ src/db/schema/guestbook-entries.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import users from "./users";
@@ -9,15 +10,29 @@ const guestbookEntries = pgTable("guestbook-entries", {
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
     message: text("message").notNull(),
-
     createdAt: timestamp("created_at", { mode: "date" }).notNull().defaultNow(),
 
 
 });
 
 // Schema for inserting a user - can be used to validate API requests
-export const insertGuestBookEntriesSchema = createInsertSchema(guestbookEntries).omit({ id: true , userId: true, createdAt: true });
-// Schema for selecting a user - can be used to validate API responses
+export const insertGuestBookEntriesSchema = createInsertSchema(guestbookEntries).omit({ 
+  id: true , 
+  userId: true, 
+  createdAt: true 
+});
+
+export const guestbookEntriesRelationships = relations(
+  guestbookEntries,
+  ({ one }) => ({
+    user: one(users, {
+      fields: [guestbookEntries.userId],
+      references: [users.id],
+    }),
+  })
+);
+
+// Schema for selecting a GuestBookEntry - can be used to validate API responses
 export const selectGuestBookEntriesSchema = createSelectSchema(guestbookEntries);
 
 export default guestbookEntries;
